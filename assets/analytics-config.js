910574371a74a325a407a90b356bf2499e1c8e06
lixa369 <zzx714520@gmail.com>
2026-08-31 18:50:18 +0800

    Add files via upload

--- assets/analytics-config.js
+++ assets/analytics-config.js
@@ -1,5 +1,5 @@
 // Fill these public site identifiers before deployment. Leave them empty to disable analytics.
 export const analyticsConfig = Object.freeze({
-  cloudflareWebAnalyticsToken: 89cea27ac0504ebe85515369ec4b20e8'',
-  goatCounterEndpoint: ''
+  cloudflareWebAnalyticsToken: '89cea27ac0504ebe85515369ec4b20e8',
+  goatCounterEndpoint: 'https://x-realhourlycalculator.goatcounter.com/count'
 });
